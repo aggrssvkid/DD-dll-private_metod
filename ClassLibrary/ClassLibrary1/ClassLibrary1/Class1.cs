9930c44af61cb81ec450ed3e3a8dffa3a04f5515
Petrovich <yaveruvdeada@mail.ru>
2022-10-12 12:55:09 +0300

    evrything

---
+++ ClassLibrary/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private
+        Dictionary<string, int> parse(IEnumerable<string> fContent)
+        {
+            // в мапу (словарь) запишем слова и их количество
+            Dictionary<string, int> word_num_pairs = new Dictionary<string, int>();
+            //в массив запишем все символы, которые не могут войти в состав слова (возможно, сюда надо что-то еще добавить)
+            char[] delimeters = { ')', '(', ']', '[', '{', '}', ' ', ',', '.', '!', '?', ':', ';', '-', '—' };
+
+            foreach (var str in fContent)
+            {
+                // в цикле проходим по всем листам, сплитим строки файла по указанным символам.
+                // Если слова нет в словаре, то добавляем его туда. Если есть, то +1 к счетчику этого слова
+                var words = str.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (word_num_pairs.ContainsKey(word) == false)
+                    {
+                        word_num_pairs.Add(word, 1);
+                    }
+                    else
+                    {
+                        word_num_pairs[word]++;
+                    }
+                }
+            }
+            return word_num_pairs;
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp1;
+
+/*public class kek
+{
+    public
+        int x = 5;
+    public
+        int z = 10;
+    private
+        int _y = 3;
+}*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        // принимаем только 1 аргумент на вход - имя файла
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Please, enter just one input file");
+            return;
+        }
+        // проверка на существование файла по указанному пути
+        string input = args[0];
+        if (File.Exists(input) == false)
+        {
+            Console.WriteLine("Your file doesnt exist! :(");
+            return;
+        }
+        // записываем файл 
+        var fContent = File.ReadLines(input);
+        // в эту штуковину записываются все аргументы метода, насколько я понял
+        Object[] arguments = { fContent };
+        // создаем экземпляр класса и определяем его тип, чтобы далее получить нужный метод
+        var obj = new ClassLibrary1.Class1();
+        
+        var type = typeof(ClassLibrary1.Class1);
+        // получаем метод инфо
+        var m_info = type.GetMethod("parse", BindingFlags.NonPublic | BindingFlags.Instance);
+        // вызываем полученный метод
+        var res = (Dictionary<string, int>) m_info.Invoke(obj, arguments);
+
+
+        if (res == null)
+        {
+            Console.WriteLine("GOODBUy!");
+            return;
+        }
+
+        var sort_words = res.OrderByDescending(_ => _.Value);
+        using (StreamWriter file = new StreamWriter("output.txt"))
+        {
+            foreach (var row in sort_words)
+            {
+                file.WriteLine($"{row.Key} {row.Value}");
+            }
+        }
+        Console.WriteLine("Complited!");
+    }
+}
